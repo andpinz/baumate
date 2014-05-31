f9f73c36c7aed691b7e9951bdfda328f68dc3a8a
jose <jose@JOSE-PC>
2014-05-31 16:58:44 -0500

    corrijo error que se presenta en la toma de los valores de las cookies

--- baumate10/web/js/ControlCookies.js
+++ baumate10/web/js/ControlCookies.js
@@ -1,32 +1,34 @@
-function ControlCookies(){
-    this.guardar = function(nombre,valor){
-        var name=nombre;
+function ControlCookies() {
+    this.guardar = function(nombre, valor) {
+        var name = nombre;
         var value = valor;
-        document.cookie = name + "=" + escape(value)+"; ";
+        document.cookie = name + "=" + escape(value) + "; ";
     }
-    this.eliminar = function(nombre){
-        var name=nombre;
-        var value= "";
-        document.cookie = name + "=" + escape(value)+"; expires=Thu, 01-Jan-70 00:00:01 GMT " ;
-    
+    this.eliminar = function(nombre) {
+        var name = nombre;
+        var value = "";
+        document.cookie = name + "=" + escape(value) + "; expires=Thu, 01-Jan-70 00:00:01 GMT ";
+
     }
-    this.consultar = function(nombre){
+    this.consultar = function(nombre) {
         var name = nombre;
         var ca = document.cookie.split(';');
-        for(var i=0; i<ca.length;) 
+        for (var i = 0; i < ca.length; )
         {
             var c = ca[i].trim();
-            if (c.indexOf(name)==0) return c.substring(name.length,c.length);
-            var valorcookie= c.split("=");
-            return  valorcookie[1];
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+                var valorcookie = c.split("=");
+                return  valorcookie[1];
+            }
             //alert("cooki obtenida!");
         }
         return "";
     }
-    this.consultarRefinado = function(nombre){
+    this.consultarRefinado = function(nombre) {
         var str = this.consultar(nombre);
-        if(str != "" && str != undefined){
-            str = str.replace("=","");
+        if (str != "" && str != undefined) {
+            str = str.replace("=", "");
         }
         return str;
     }
